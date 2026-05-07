3cc7b0ec01b0922cb65ab23d4c0855580975b20c
danielmlullullangi <daniel.m.lullulangi@gmail.com>
2026-05-07 14:14:55 +0700

    update

--- langgraph_fast_api/routing/routing_core.py
+++ langgraph_fast_api/routing/routing_core.py
@@ -2,43 +2,47 @@ from haversine import haversine
 import pandas as pd
 from routing.tsp_solver import intracluster_tsp
 
-def get_cluster_endpoints(
-        coordinates_map: dict[str, tuple[float, float]],
+
+"""
+
+"""
+def tentukan_titik_start_dan_end(
+        koordinat_lokasi_pada_peta: dict[str, tuple[float, float]],
         scores_map: dict[str, int],
         ordered: list[str],
 ) -> tuple[int, int]:
     """
     Menentukan start dan end point berdasarkan tempat dengan rating tertinggi atau terdekat
     """
-    n = len(ordered)
-    if n == 0:
+    jumlah_tempat_dalam_cluster = len(ordered)
+    if jumlah_tempat_dalam_cluster == 0:
         return -1, -1 # Menandakan tidak ada tempat dalam cluster
-    if n == 1:
+    if jumlah_tempat_dalam_cluster == 1:
         return 0, 0   # Hanya 1 tempat, titik start dan end di tempat yang sama
 
-    start_idx, end_idx = -1, -1
+    indeks_titik_pertama, indeks_titik_terakhir = -1, -1
 
     # Menentukan start_idx
     # Cluster (hari) pertama
-    start_idx = ordered.index(max(ordered, key=lambda p: scores_map.get(p, 0))) # Heuristic: mengambil tempat dengan rating tertinggi
+    indeks_titik_pertama = ordered.index(max(ordered, key=lambda p: scores_map.get(p, 0))) # Heuristic: mengambil tempat dengan rating tertinggi
 
     # Mencari end_idx dengan titik terjauh dengan start_idx
-    start_coord = coordinates_map[ordered[start_idx]]
-    dist = -1
+    start_coord = koordinat_lokasi_pada_peta[ordered[indeks_titik_pertama]]
+    jarak = -1
 
-    for i in range(n):
-        if i == start_idx:
+    for tempat in range(jumlah_tempat_dalam_cluster):
+        if tempat == indeks_titik_pertama:
             continue
 
-        d = haversine(start_coord, coordinates_map[ordered[i]])
-        if d > dist:
-            dist = d
-            end_idx = i
+        d = haversine(start_coord, koordinat_lokasi_pada_peta[ordered[tempat]])
+        if d > jarak:
+            jarak = d
+            indeks_titik_terakhir = tempat
 
-    return start_idx, end_idx
+    return indeks_titik_pertama, indeks_titik_terakhir
 
 def get_distance_matrix(
-        coordinates_map: dict[str, tuple[float, float]],
+        koordinat_lokasi_pada_peta: dict[str, tuple[float, float]],
         places: list[str]
 ) -> list[list[float]]:
     """
@@ -46,7 +50,7 @@ def get_distance_matrix(
     tempat yang sudah diketahui
     """
     n = len(places)
-    coords = [coordinates_map[p] for p in places]
+    coords = [koordinat_lokasi_pada_peta[p] for p in places]
 
     matrix = [[0.0] * n for _ in range(n)]
 
@@ -121,7 +125,7 @@ async def destination_routing(
         P[1] = places
         return P, 0.0
 
-    start, end = get_cluster_endpoints(
+    start, end = tentukan_titik_start_dan_end(
             coordinates_map,
             scores_map,
             places
